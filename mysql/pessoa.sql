88e272825fa20f56176053281cc587381e434013
Michelle Barreto <33093587+Lynlium@users.noreply.github.com>
2022-07-05 19:10:48 -0300

    Update pessoa.sql

--- mysql/pessoa.sql
+++ mysql/pessoa.sql
@@ -4,6 +4,17 @@ CREATE TABLE pessoas {
     nascimento DATE
 }
 
-INSERT INTO pessoas( nome, nascimento)VALUES ('Nathally', '1990/05/22')
-INSERT INTO pessoas( nome, nascimento)VALUES ('Felipe', '1995/07/17')
-INSERT INTO pessoas( nome, nascimento)VALUES ('Marcela', '1998/12/02')
+INSERT INTO pessoas( nome, nascimento) VALUES ('Nathally', '1990/05/22')
+INSERT INTO pessoas( nome, nascimento) VALUES ('Felipe', '1995/07/17')
+INSERT INTO pessoas( nome, nascimento) VALUES ('Marcela', '1998/12/02')
+
+/* Buscar nas tabelas */
+
+SELECT * FROM pessoas
+
+SELECT nome FROM pessoas
+
+/* Atualizar dados na tabela */
+
+UPDATE pessoas SET nome = 'Nathaly Souza'; /* muda todos os nomes da coluna */
+UPDATE pessoas SET nome = 'Nathay Souza' WHERE id = 1;
