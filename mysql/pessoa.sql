985676e2e703d3e0baf78cebac2501641b14c880
Michelle Barreto <33093587+Lynlium@users.noreply.github.com>
2022-07-05 19:27:46 -0300

    Update pessoa.sql

--- mysql/pessoa.sql
+++ mysql/pessoa.sql
@@ -1,7 +1,8 @@
 CREATE TABLE pessoas {
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR (30) NOT NULL,
-    nascimento DATE
+    nascimento DATE,
+    genero VARCHAR(1) NOT NULL
 }
 
 INSERT INTO pessoas( nome, nascimento) VALUES ('Nathally', '1990/05/22')
@@ -18,3 +19,26 @@ SELECT nome FROM pessoas
 
 UPDATE pessoas SET nome = 'Nathaly Souza'; /* muda todos os nomes da coluna */
 UPDATE pessoas SET nome = 'Nathay Souza' WHERE id = 1;
+
+/* Deletar informações */
+
+SELECT * FROM pessoas WHERE id = 3;
+
+DELETE FROM pessoas WHERE  id=3;
+
+/* Ordenar dados */
+
+SELECT * FROM pessoas ORDER BY nome;
+SELECT * FROM pessoas ORDER BY nome DESC;
+
+/* Alterar e atualizar dados */
+
+ALTER TABLE 'pessoas' ADD genero VARCHAR (1) NOT NULL AFTER 'nascimento';
+
+UPDATE pessoas SET genero='m' WHERE id=2;
+UPDATE pessoas SET genero='F' WHERE id=1;
+UPDATE pessoas SET genero='F' WHERE id=3;
+
+/* Agrupar dados */
+
+SELECT COUNT(id) FROM pessoas GROUP BY genero;
