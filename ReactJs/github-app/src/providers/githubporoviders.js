9ee8eba97f7b43685d10a943907420fc5160784f
Michelle Barreto <33093587+Lynlium@users.noreply.github.com>
2022-07-05 10:56:50 -0300

    Create githubporoviders.js

---
+++ ReactJs/github-app/src/providers/githubporoviders.js
@@ -0,0 +1,7 @@
+export const GithubProvider = () =>{
+    fetch (process.env.REACT_APP_API).then((reponse) => reponse.json());
+
+    }
+
+
+    //método de  API export baseado no server de naruto quotes, pode não funcionar!!//
